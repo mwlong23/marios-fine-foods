2faea1d710423d79e3491558d432097d71da1d2a
Mitchell <meechllada@gmail.com>
2017-11-17 11:10:19 -0800

    create views needed: index, new, show, edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
 end
