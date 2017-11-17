ec289da55f346a9a188a34341a06ffb23d8e35db
Mitchell <meechllada@gmail.com>
2017-11-17 09:45:38 -0800

    add all passing validation tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :description, :presence => true
+  validates :country_of_origin, :presence => true
+  validates :price, :presence => true
+  validates :created_at, :presence => true
+  validates :updated_at, :presence => true
 
 end
