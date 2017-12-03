288062d7fa5b93339f3dcfb0dcff674f653a3707
Mitchell <meechllada@gmail.com>
2017-12-03 14:15:27 -0800

    Add domestic products to welcome view

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,14 +6,15 @@ class Product < ActiveRecord::Base
   validates :price, :presence => true
   has_many :reviews
 
-  scope :made_in_usa, -> { where("country_of_origin ==?", "USA")}
+  scope :made_in_usa, -> { where(country_of_origin: "USA")}
 
   scope :most_reviews, -> {(
-  select("products.id, products.name, products.description, count(reviews.id) as reviews_count")
-  .joins(:reviews)
-  .group("products.id")
-  .order("reviews_count DESC")
-  .limit(10)
+    select("products.id, products.name, products.description, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(10)
   )}
 
+  scope :recently_added, -> { order(created_at: :desc).limit(3)}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,6 @@ class Review < ActiveRecord::Base
   validates :username, :presence => true
   validates :rating, :presence => true
   validates :written_review, :presence => true
-  # validates :created_at, :optional => true
-  # validates :updated_at, :optional => true
 
   belongs_to :product
 end
