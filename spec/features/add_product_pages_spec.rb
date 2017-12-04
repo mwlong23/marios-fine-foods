81bf470bdf5e0b0a295d21738ce8c823b05546dd
Mitchell <meechllada@gmail.com>
2017-12-03 21:43:50 -0800

    add tests for Error messages for data validations

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -25,5 +25,36 @@ describe "the add a product process" do
     expect(page).to have_content 'Name can\'t be blank'
   end
 
+  it "Checks error messaging when adding a new product" do
+    visit products_path
+    click_link 'Add A Product'
+    fill_in 'Name', :with => 'Calculator'
+    fill_in 'Description', :with => ''
+    fill_in 'Country of origin', :with => 'USA'
+    fill_in 'Price', :with => 5.00
+    click_on 'Create Product'
+    expect(page).to have_content 'Description can\'t be blank'
+  end
 
+  it "Checks error messaging when adding a new product" do
+    visit products_path
+    click_link 'Add A Product'
+    fill_in 'Name', :with => 'Calculator'
+    fill_in 'Description', :with => 'A small computer for calculations'
+    fill_in 'Country of origin', :with => ''
+    fill_in 'Price', :with => 5.00
+    click_on 'Create Product'
+    expect(page).to have_content 'Country of origin can\'t be blank'
+  end
+  #
+  it "Checks error messaging when adding a new product" do
+    visit products_path
+    click_link 'Add A Product'
+    fill_in 'Name', :with => 'Calculator'
+    fill_in 'Description', :with => 'A small computer for calculations'
+    fill_in 'Country of origin', :with => 'USA'
+    fill_in 'Price', :with => ""
+    click_on 'Create Product'
+    expect(page).to have_content 'Price can\'t be blank'
+  end
 end
