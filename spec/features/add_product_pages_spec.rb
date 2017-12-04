6a309db7bf1230f2ac1f416dfb11a2bbdf5651b2
Mitchell <meechllada@gmail.com>
2017-12-03 21:33:32 -0800

    passes last failing test, adds new failing test

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -13,4 +13,17 @@ describe "the add a product process" do
     expect(page).to have_content 'Calculator'
     expect(page).to have_content 'A small computer for calculations'
   end
+
+  it "Checks error messaging when adding a new product" do
+    visit products_path
+    click_link 'Add A Product'
+    fill_in 'Name', :with => ''
+    fill_in 'Description', :with => 'A small computer for calculations'
+    fill_in 'Country of origin', :with => 'USA'
+    fill_in 'Price', :with => 5.00
+    click_on 'Create Product'
+    expect(page).to have_content 'Name can\'t be blank'
+  end
+
+
 end
